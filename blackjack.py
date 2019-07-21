6e8fbda3c2f2896835d72cbb0ebe3eaf6cd1f725
kevin.home <kevin@home.com>
2019-07-21 18:40:26 +0800

    revised the two files

--- blackjack.py
+++ blackjack.py
@@ -121,6 +121,10 @@ def new_game():
 def shuffle():
     random.shuffle(deck)
 
+
+def kevin():
+    print("kevin says hello")
+
 mainWindow = tkinter.Tk()
 
 # Set up the screen and frames for the dealer and player
